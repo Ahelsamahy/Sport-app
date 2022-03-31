7546e79bdaab6055d69113334bdaa5bc389884f2
AhmedMahfouz <ahmelsamahy@gmail.com>
2022-03-31 17:00:35 +0200

    implemented sidebar
    with the required navigation tabs for

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sport_app/Side_NavBar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -11,6 +12,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: NavBar(),
       appBar: AppBar(
         title: const Text('Welcome to the app'),
       ),
---
+++ lib/Side_NavBar.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class NavBar extends StatelessWidget {
+  const NavBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        // Remove padding
+        padding: EdgeInsets.zero,
+        children: [
+          UserAccountsDrawerHeader(
+            accountName: Text('Ahmed Mahfouz'),
+            accountEmail: Text('Ahmed@gmail.com'),
+            currentAccountPicture: CircleAvatar(
+              child: ClipOval(
+                child: Image.asset(
+                  'android/assets/images/black&white_circle.jpg',
+                  fit: BoxFit.cover,
+                  width: 90,
+                  height: 90,
+                ),
+              ),
+            ),
+            decoration: const BoxDecoration(
+              color: Colors.blue,
+              image: DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage('android/assets/images/NightCall.jpg')),
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text('Dashboard'),
+            onTap: () => null,
+          ),
+          ListTile(
+            leading: Icon(Icons.share),
+            title: Text('Share'),
+            onTap: () => null,
+          ),
+          const ListTile(
+            leading: Icon(Icons.notifications),
+            title: Text('Request'),
+          ),
+          // ignore:
+          Divider(),
+          ListTile(
+            leading: const Icon(Icons.settings),
+            title: Text('Settings'),
+            onTap: () => null,
+          ),
+          Divider(),
+          ListTile(
+            title: Text('Exit'),
+            leading: Icon(Icons.exit_to_app),
+            onTap: () => null,
+          ),
+        ],
+      ),
+    );
+  }
+}
