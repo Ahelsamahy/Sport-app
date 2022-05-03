14646dae51b2025c8be845f55e7d04689966a894
yslamgulypirgulyyev <islamguly28@gmail.com>
2022-05-03 22:37:13 +0200

    Exit button dialog

---
+++ lib/Exit.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Exit extends StatefulWidget {
+  @override
+  _ExitState createState() => _ExitState();
+}
+
+class _ExitState extends State<Exit> {}
